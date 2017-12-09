77961a6444df0e69b9495e34a2bcbd57cd6fd72d
RomanKuzhekov <profet@inbox.ru>
2017-12-09 22:05:46 +0300

    Реализовал паттерн Синглтон

--- traits/TSingleton.php
+++ traits/TSingleton.php
@@ -11,5 +11,16 @@ namespace app\traits;
 
 trait TSingleton
 {
+    private static $instance = null;
 
+    public function __construct(){}
+    public function __clone(){}
+    public function __wakeup(){}
+
+    public static function getInstance(){
+        if(is_null(static::$instance)){
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
 }
