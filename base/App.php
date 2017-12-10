bfd06900545dcf67613855dc92d7c1c75dfc4895
RomanKuzhekov <profet@inbox.ru>
2017-12-10 23:26:31 +0300

    Реализовал главный класс App для запуска приложения из index.php

--- base/App.php
+++ base/App.php
@@ -17,6 +17,10 @@ use app\traits\TSingleton;
 /**
  * Class App
  * @package app\base
+ * Используем Синглтон для одиночного подключения класса
+ * автозагрузчик файлов
+ * используем магический метод get для подключения компонентов прописанных в конфиге
+ * запускаем FrontController для обработки строки(controller/action/id) переданной в бразуер
  * @property Controller main
  * @property Db db
  */
@@ -25,49 +29,56 @@ class App
     use TSingleton;
 
     public $config;
-
-    /** @var  Container */
+    private $items = [];
     private $components;
 
-    public static function call(){
+    public static function call()
+    {
         return static::getInstance();
     }
 
-    public function run(){
+    public function run()
+    {
         //подгружаем массив конфига
         $this->config = include "../config/config.php";
         //запускаем автозагрузчик файлов
         $this->autoloadRegister();
-        //подгружаем класс для записи и получения экземпляров классов в конфиге
-        $this->components = new Container();
         //запускаем FrontController
         $this->main->runAction();
     }
 
-    private function autoloadRegister(){
+    private function autoloadRegister()
+    {
         require '../services/Autoloader.php';
         spl_autoload_register([new \app\services\Autoloader(), "loadClass"]);
     }
 
     public function __get($name)
     {
-        return $this->components->get($name);
+        return $this->get($name);
     }
 
+    private function get($key)
+    {
+        if (!isset($this->items[$key])) {
+            $this->items[$key] = $this->createComponent($key);
+        }
+        return $this->items[$key];
+    }
 
-    public function createComponent($name){
-        if(isset($this->config['components'][$name])){
+    private function createComponent($name)
+    {
+        if (isset($this->config['components'][$name])) {
             $params = $this->config['components'][$name];
             $className = $params['class'];
-            if(class_exists($className)){
+            if (class_exists($className)) {
                 //реализуем подгрузку компонента для создания экзмепляра и загрузки свойств в конструктор(например для класса Db)
                 unset($params['class']);
                 $reflection = new \ReflectionClass($className);
-                return $reflection->newInstanceArgs($params);
+                return $this->components = $reflection->newInstanceArgs($params);
             }
+            throw new \Exception("Класс $className не был найден");
         }
         throw new \Exception("Компонент $name не существует в config.php");
     }
-
-
 }
