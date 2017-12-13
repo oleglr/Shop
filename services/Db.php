fa143db548adaf576bdd4053c51a4fdd13e41fdf
RomanKuzhekov <profet@inbox.ru>
2017-12-13 23:59:08 +0300

    Реализовал класс для работы с БД

--- services/Db.php
+++ services/Db.php
@@ -8,13 +8,18 @@
 
 namespace app\services;
 
-
 /**
+ * Класс для работы с БД
+ * подключение к БД - getConnection
+ * запрос на выолнение - execute
+ * Одиночный выбор записи - fetchOne
+ * Выбираем все записи - fetchAll
  * Class Db
  * @package app\services
  */
-class Db
+final class Db
 {
+    private $conn = null;
     private $config;
 
     /**
@@ -30,6 +35,58 @@ class Db
         $this->config['charset'] = $charset;
     }
 
+    private function getConnection()
+    {
+        if (is_null($this->conn)) {
+            $this->conn = new \PDO(
+                $this->prepareDsn(),
+                $this->config['login'],
+                $this->config['password']
+            );
+        }
+
+        $this->conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        return $this->conn;
+    }
+
+    private function query($sql, $params = null)
+    {
+        $PDOStatement = $this->getConnection()->prepare($sql);
+        $PDOStatement->execute($params);
+        return $PDOStatement;
+    }
+
+    public function execute($sql, $params = [])
+    {
+        $this->query($sql, $params);
+        return true;
+    }
+
+    public function fetchOne($sql, $params = [], $class)
+    {
+        $smtp = $this->query($sql, $params);
+        $smtp->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $class);
+        return $smtp->fetch();
+    }
+
+    public function fetchAll($sql, $class)
+    {
+        $smtp = $this->query($sql);
+        $smtp->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $class);
+        return $smtp->fetchAll();
+    }
+
+    private function prepareDsn()
+    {
+        return sprintf("%s:host=%s;dbname=%s;charset=%s",
+            $this->config['driver'],
+            $this->config['host'],
+            $this->config['database'],
+            $this->config['charset']
+        );
+    }
 
 
 }
